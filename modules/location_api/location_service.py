fdbfab192caab313bbbc76a28ef5b794f97cb4c0
Josh bolten <jozhbolt@gmail.com>
2021-09-22 17:54:11 +0100

    update

--- modules/location_api/location_service.py
+++ modules/location_api/location_service.py
@@ -2,7 +2,5 @@ from location_api.location_writer import create
 
 
 def create_location(location_data):
-    """
-    creates and inserts a new location into the database
-    """
+
     return create(location_data)
