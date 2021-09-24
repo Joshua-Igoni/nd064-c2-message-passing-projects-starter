cfed2c0c75f569ac69d5700db16871ae31beba03
Josh bolten <jozhbolt@gmail.com>
2021-09-24 15:26:29 +0100

    update fixes

--- modules/api/app_folder/__init__.py
+++ modules/api/app_folder/__init__.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 
 
 def create_app(env=None):
-    from app.config import config_by_name
-    from app.routes import register_routes
+    from app_folder.config import config_by_name
+    from app_folder.routes import register_routes
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
